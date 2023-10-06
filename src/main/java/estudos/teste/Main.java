e893de425ab824a65a1572f265a1f3be0bf126e2
jcorreaw <juninhocorrea2014@gmail.com>
2023-10-06 12:10:21 -0300

    Test Idade Correta

--- src/main/java/estudos/teste/Main.java
+++
@@ -1,7 +0,0 @@
-package estudos.teste;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ola mundo!");
-    }
-}
---
+++ src/test/java/estudos/teste/PessoaTeste.java
@@ -0,0 +1,15 @@
+package estudos.teste;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
+public class PessoaTeste {
+
+    @Test
+    void deveCalcularIdadeCorretamente() {
+        Pessoa jessica = new Pessoa("Jéssica", LocalDateTime.of(2000, 1, 1, 15, 0, 0));
+        Assertions.assertEquals(23, jessica.getIdade());
+    }
+}
